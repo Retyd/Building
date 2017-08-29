76951483f9a9dcf0ac774f89f8e610897c8a9403
Elena Listratova <int28h@protonmail.com>
2017-08-29 22:09:28 +0400

    a bit of officefloor

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,16 @@ import buildings.Dwelling;
 public class Main {
 
     public static void main(String[] args) {
-    	int[] flatsAmount = {1, 2, 3, 4, 5, 6, 7};
+    	/*int[] flatsAmount = {1, 2, 3, 4, 5, 6, 7};
         Dwelling newBuilding = new Dwelling(7, flatsAmount);
         
-        System.out.println(newBuilding);        
+        System.out.println(newBuilding); 
+        
+        
+        OfficeFloor one = new OfficeFloor();
+        OfficeFloor.Node nOne = one.new Node();*/
+        
+        OfficeFloor testOfficeFloor = new OfficeFloor(5);
+        System.out.println(testOfficeFloor.getFloorArea());
     }
 }
